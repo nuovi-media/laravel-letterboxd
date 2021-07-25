f5b26c8125ec1f533e8d03feb7c8a63820aed08e
Lorenzo Breda <lorenzo@lbreda.com>
2021-07-25 22:45:21 +0200

    :bug: Abstract class used for comments

--- src/Letterboxd/ListComment.php
+++ src/Letterboxd/ListComment.php
@@ -24,45 +24,9 @@ use Carbon\Carbon;
  * @property ListIdentifier $list;
  * @property string $comment;
  */
-class ListComment extends LetterboxdBaseElement
+class ListComment extends AbstractComment
 {
-    protected string $id;
-    protected MemberSummary $member;
-    protected Carbon $whenCreated;
-    protected Carbon $whenUpdated;
-    protected string $commentLbml;
-    protected bool $removedByAdmin;
-    protected bool $removedByContentOwner;
-    protected bool $deleted;
-    protected bool $blocked;
-    protected bool $blockedByOwner;
-    protected int $editableWindowExpiresIn;
     protected ListIdentifier $list;
-    protected string $comment;
-
-    /**
-     * @param MemberSummary|array $member
-     */
-    protected function setMember(MemberSummary|array $member)
-    {
-        $this->member = is_array($member) ? new MemberSummary($member) : $member;
-    }
-
-    /**
-     * @param Carbon|string $whenCreated
-     */
-    protected function setWhenCreated(Carbon|string $whenCreated)
-    {
-        $this->whenCreated = is_string($whenCreated) ? Carbon::parse($whenCreated) : $whenCreated;
-    }
-
-    /**
-     * @param Carbon|string $whenUpdated
-     */
-    protected function setWhenUpdated(Carbon|string $whenUpdated)
-    {
-        $this->whenUpdated = is_string($whenUpdated) ? Carbon::parse($whenUpdated) : $whenUpdated;
-    }
 
     /**
      * @param ListIdentifier|array $list
