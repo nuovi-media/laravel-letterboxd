f96e56fa4436397d3b1acaafaf261958cb2b9479
Lorenzo Breda <lorenzo@lbreda.com>
2021-07-27 21:41:50 +0200

    :bug: null default for uninitialised properties

--- src/Letterboxd/LetterboxdBaseElement.php
+++ src/Letterboxd/LetterboxdBaseElement.php
@@ -21,7 +21,7 @@ abstract class LetterboxdBaseElement
         $var = lcfirst(substr($method, 3));
 
         if (strncasecmp($method, "get", 3) === 0) {
-            return $this->$var;
+            return $this->$var ?? null;
         }
         if (strncasecmp($method, "set", 3) === 0) {
             $this->$var = $params[0];
